df9553b521dae487ffa95b76111d980f64106690
Masoud Sattari <masoud.sattari@argedor.com>
2020-02-18 18:10:44 +0300

    Add controller test class

---
+++ src/test/java/com/masoud/reactivejava/LinkControllerTest.java
@@ -0,0 +1,8 @@
+package com.masoud.reactivejava;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class LinkControllerTest {
+}
