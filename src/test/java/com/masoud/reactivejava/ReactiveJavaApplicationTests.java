52dd224104cf7d4785409b0d6442d694a33a5447
Masoud Sattari <masoud.sattari@argedor.com>
2020-02-18 17:44:41 +0300

    Initial commit

---
+++ src/test/java/com/masoud/reactivejava/ReactiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masoud.reactivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
